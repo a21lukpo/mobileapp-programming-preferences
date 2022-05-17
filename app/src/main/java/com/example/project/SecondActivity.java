c1320a592699c001c2ac98e95dee94f988b9202f
a21lukpo <a21lukpo@student.his.se>
2022-05-17 21:47:42 +0200

    jag gjorde sparfunktion i min SecondActivity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,33 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button back;
+    EditText setUsername;
+    Button save;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        save = findViewById(R.id.save);
+        setUsername = findViewById(R.id.setUsername);
+        preferences = getSharedPreferences(("preferences"), MODE_PRIVATE);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("username", setUsername.getText().toString());
+                editor.apply();
+            }
+        });
     }
 }
