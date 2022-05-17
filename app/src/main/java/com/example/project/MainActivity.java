f466db2927015a29fefefcb40f7244461242e4f7
a21lukpo <a21lukpo@student.his.se>
2022-05-17 21:57:04 +0200

    skapade presentation för användarnamnet samt gjorde en intent för att knappen ska fungera

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences preferences;
+    TextView showUsername;
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        preferences = getSharedPreferences(("preferences"), MODE_PRIVATE);
+        showUsername = findViewById(R.id.showUsername);
+        next = findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String username = preferences.getString("username", "jag glum");
+        showUsername.setText(username);
+    }
 }
