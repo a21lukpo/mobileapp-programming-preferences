f3ff405b894f9ecd8152b24a0a35308ad888e98d
a21lukpo <a21lukpo@student.his.se>
2022-05-17 22:02:23 +0200

    Jag la till en intent i SecondActivity som tar användaren tillbaka till MainActivity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +20,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         save = findViewById(R.id.save);
         setUsername = findViewById(R.id.setUsername);
         preferences = getSharedPreferences(("preferences"), MODE_PRIVATE);
